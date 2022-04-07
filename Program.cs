85d6bbe1d06022b856374a080b11554a9c1010bb
blue <d33pblue96@gmail.com>
2022-04-07 10:21:20 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.IO.Pipes;
+
+namespace IPC_test
+{
+    public class StreamString
+    {
+        private Stream ioStream;
+        private UnicodeEncoding streamEncoding;
+
+        public StreamString(Stream ioStream)
+        {
+            this.ioStream = ioStream;
+            streamEncoding = new UnicodeEncoding();
+        }
+
+        public string ReadString()
+        {
+            int len;
+            len = ioStream.ReadByte() * 256;
+            len += ioStream.ReadByte();
+            var inBuffer = new byte[len];
+            ioStream.Read(inBuffer, 0, len);
+
+            return streamEncoding.GetString(inBuffer);
+        }
+
+        public int WriteString(string outString)
+        {
+            byte[] outBuffer = streamEncoding.GetBytes(outString);
+            int len = outBuffer.Length;
+            if (len > UInt16.MaxValue)
+            {
+                len = (int)UInt16.MaxValue;
+            }
+            ioStream.WriteByte((byte)(len / 256));
+            ioStream.WriteByte((byte)(len & 255));
+            ioStream.Write(outBuffer, 0, len);
+            ioStream.Flush();
+
+            return outBuffer.Length + 2;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeClientStream clientStream = new NamedPipeClientStream(".","pipe", PipeDirection.InOut);
+            clientStream.Connect();
+            Console.WriteLine(new StreamString(clientStream).ReadString());
+            Console.Read();
+        }
+    }
+}
